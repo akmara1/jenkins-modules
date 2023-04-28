8e0b7c080898bf3448c1d8e401e6a8cc574b6f62
akmara1 <95084923+akmara1@users.noreply.github.com>
2023-04-28 23:22:01 +0600

    Update launch_template.tf

--- eks/launch_template.tf
+++ eks/launch_template.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "example" {
   name          = "akmarals"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
 
   tags = {
     "environment" = "${var.project}"
