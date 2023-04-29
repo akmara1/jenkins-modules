380f1b6330983d737fa2d9bdd8c7e18a1113c8fd
akmara1 <95084923+akmara1@users.noreply.github.com>
2023-04-29 18:37:34 +0600

    Update var.tf

--- vpc/var.tf
+++ vpc/var.tf
@@ -1,11 +1,5 @@
 variable "aws_availability_zones" {
-  default     = [
-│     "us-east-1a",
-│     "us-east-1b",
-│     "us-east-1c",
-│     "us-east-1d",
-│     "us-east-1f"
-│   ]
+  default     = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1f"]
 }
 
 variable "region" {
